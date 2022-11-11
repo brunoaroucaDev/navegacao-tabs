18916f5084f8eb801c7a2070213c202d114c01fe
Bruno Arouca <brunoarouca.dev@gmail.com>
2022-11-11 19:23:38 -0300

    Projeto Finalizado

---
+++ assets/js/script.js
@@ -0,0 +1,89 @@
+import newsJson from "../../newsJson.js";
+
+const dataJson = newsJson;
+console.log(dataJson)
+const navTabs = document.querySelectorAll('.nav-tab li a');
+
+
+newsRender(0)
+
+navTabs.forEach((nav, index) => {
+
+    nav.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        navTabs.forEach(navItem => {
+            navItem.classList.remove('active')
+            document.querySelector('.tab-content').innerHTML = ''
+        })
+        
+        newsRender(index)
+        nav.classList.add('active')
+
+    })
+
+    
+})
+
+function newsRender(navTabIndex){
+    let navTab = '';
+    switch(navTabIndex){
+        case 0:
+        navTab = 'jornalismo';
+        break;
+        case 1:
+        navTab = 'esporte';
+        break;
+        case 2:
+        navTab = 'entretenimento';
+        break;
+        case 3:
+        navTab = 'tecnologia';
+        break;
+    }
+
+     
+    dataJson.forEach((news) => {
+        
+        if ( navTab === news.category){
+            
+          let html = `
+          <div class="tab-pane active">
+                <img src="./assets/media/${news.image}" alt="">
+                <p>${news.title.substring(0, 64)}...</p>
+                <span class="notice-date">${news.date} ${news.hour}</span>
+                <div class="btn">
+                    <a href="">Leia mais</a>
+                    <span class="arrow"></span>
+                </div>
+            </div>
+          `  
+        document.querySelector('.tab-content').innerHTML += html
+        
+        }
+    })
+}
+const setaBtn = document.querySelectorAll('.tab-content .tab-pane .btn span');
+const linkBtn = document.querySelectorAll('.tab-content .tab-pane .btn a');
+
+linkBtn.forEach((link, linkIndex) => {
+    link.addEventListener('mouseover', () => {
+       
+        setaBtn.forEach((seta, setaIndex) => {
+            
+            if (setaIndex === linkIndex){
+               seta.style.marginLeft = '40px'
+            }
+        })
+    })
+    link.addEventListener('mouseout', () => {
+       
+        setaBtn.forEach((seta, setaIndex) => {
+            
+            if (setaIndex === linkIndex){
+                seta.style.marginLeft = '20px'
+            }
+        })
+    })
+})
+
---
+++ newsJson.js
@@ -0,0 +1,105 @@
+let newsJson = [
+    {
+        id: 1,
+        category: "jornalismo",
+        image: "imagem1.jpeg",
+        title: "Alertas de desmate na Amazônia têm pior outubro da série histórica",
+        date: "11/11/2022",
+        hour: "05h30",   
+    },
+    {
+        id: 2,
+        category: "jornalismo",
+        image: "imagem2.jpeg",
+        title: "Reforma trabalhista faz 5 anos sem gerar empregos previstos",
+        date: "10/10/2022",
+        hour: "07h29",   
+    },
+    {
+        id: 3,
+        category: "jornalismo",
+        image: "imagem3.jpeg",
+        title: "Prazo para atualização do CadÚnico acaba hoje; veja o que fazer",
+        date: "10/10/2022",
+        hour: "10h40",   
+    },
+    {
+        id: 4,
+        category: "esporte",
+        image: "imagem4.jpeg",
+        title: "Analistas de mercado apostam no Brasil como campeão na Copa do Mundo",
+        date: "10/11/2022",
+        hour: "10h40",   
+    },
+    {
+        id: 5,
+        category: "esporte",
+        image: "imagem5.jpeg",
+        title: "Brasil conquista título mundial inédito no vôlei sentado feminino",
+        date: "10/11/2022",
+        hour: "11h40",   
+    },
+    {
+        id: 6,
+        category: "esporte",
+        image: "imagem6.jpeg",
+        title: "LeBron reconhece erro de Kyrie, mas considera punição excessiva",
+        date: "10/11/2022",
+        hour: "11h40",   
+    },
+    {
+        id: 7,
+        category: "entretenimento",
+        image: "imagem7.jpeg",
+        title: "Danilo tripudia de Moa em Cara e Coragem: 'Eu fiz ser o mais agradável possível",
+        date: "12/11/2022",
+        hour: "09h40",   
+    },
+    {
+        id: 8,
+        category: "entretenimento",
+        image: "imagem8.jpeg",
+        title: "Marília Gabriela posa como filho e o ex-genro em evento, em São Paulo",
+        date: "10/11/2022",
+        hour: "11h40",   
+    },
+    {
+        id: 9,
+        category: "entretenimento",
+        image: "imagem9.jpeg",
+        title: "IZA passa faixa de rainha de bateria da Imperatriz Leopoldinense para passista do Complexo do Alemão",
+        date: "09/11/2022",
+        hour: "10h40",   
+    },
+    {
+        id: 10,
+        category: "tecnologia",
+        image: "imagem10.jpg",
+        title: "Apple deve iniciar produção de óculos de realidade aumentada em 2023",
+        date: "09/11/2022",
+        hour: "07h10",   
+    },
+    {
+        id: 11,
+        category: "tecnologia",
+        image: "imagem11.jpg",
+        title: "Celular por até R$ 1 mil: confira cinco modelos lançados em 2022",
+        date: "09/11/2022",
+        hour: "07h10",   
+    },
+    {
+        id: 12,
+        category: "tecnologia",
+        image: "imagem12.jpg",
+        title: "5G funciona em qualquer celular? Entenda requisitos para usar a rede",
+        date: "01/11/2022",
+        hour: "12h30",   
+    }
+    
+
+    
+]
+
+
+
+export default newsJson;
